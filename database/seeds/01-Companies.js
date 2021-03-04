c49ddd809fa1c71f3fe691aced63d246353acdee
David Molinari <david.m.molinari@gmail.com>
2021-03-04 13:04:55 -0500

    Updates to seeds and schema

--- database/seeds/01-Companies.js
+++ database/seeds/01-Companies.js
@@ -1,10 +1,9 @@
-
 exports.seed = function(knex) {
   return knex('Companies').del()
     .then(function() {
       return knex('Companies').insert([
-        {id: 1, URL: 'energyti.me', Socials: 'david@energyti.me'},
-        {id: 2, URL: 'saleti.me', Socials: 'tommy@saleti.me'}
+        {id: 1, URL: 'energyti.me', Contact: 'david@energyti.me'},
+        {id: 2, URL: 'saleti.me', Contact: 'tommy@saleti.me'}
       ]);
     });
 };
