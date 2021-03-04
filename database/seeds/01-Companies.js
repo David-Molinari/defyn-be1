ad6764fe0594e25746fef5ededfdc07a700a2c83
David Molinari <david.m.molinari@gmail.com>
2021-03-04 13:15:05 -0500

    Updates to seeds and schema

--- database/seeds/01-Companies.js
+++ database/seeds/01-Companies.js
@@ -2,8 +2,9 @@ exports.seed = function(knex) {
   return knex('Companies').del()
     .then(function() {
       return knex('Companies').insert([
-        {id: 1, URL: 'energyti.me', Contact: 'david@energyti.me'},
-        {id: 2, URL: 'saleti.me', Contact: 'tommy@saleti.me'}
+        {id: 1, URL: 'defyn.co', Contact: 'hi@defyn.co'},
+        {id: 2, URL: 'energyti.me', Contact: 'david@energyti.me'},
+        {id: 3, URL: 'saleti.me', Contact: 'tommy@saleti.me'}
       ]);
     });
 };
--- database/seeds/02-Videos.js
+++ database/seeds/02-Videos.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
       return knex('Videos').insert([
         {id: 1, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 1},
         {id: 2, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 2},
-        {id: 3, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 1},
-        {id: 4, Name: 'You see', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 1},
-        {id: 5, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 2},
+        {id: 3, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
+        {id: 4, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 2},
+        {id: 5, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 3},
       ]);
     });
 };
