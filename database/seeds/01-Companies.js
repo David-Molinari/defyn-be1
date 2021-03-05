8f50ff6c5fba0e6de2d47d17e86deadc8f549855
David Molinari <david.m.molinari@gmail.com>
2021-03-05 17:33:54 -0500

    Adding name in place contact

--- database/seeds/01-Companies.js
+++ database/seeds/01-Companies.js
@@ -2,9 +2,11 @@ exports.seed = function(knex) {
   return knex('Companies').del()
     .then(function() {
       return knex('Companies').insert([
-        {id: 1, URL: 'defyn.co', Contact: 'hi@defyn.co'},
-        {id: 2, URL: 'energyti.me', Contact: 'david@energyti.me'},
-        {id: 3, URL: 'saleti.me', Contact: 'tommy@saleti.me'}
+        {id: 1, URL: 'defyn.co', Name: 'Defyn'},
+        {id: 2, URL: 'energyti.me', Name: 'EnergyTime'},
+        {id: 3, URL: 'saleti.me', Name: 'SaleTime'},
+        {id: 4, URL: 'yoursand.us', Name: 'YoursAndUs'},
+        {id: 5, URL: 'localhost:3000', Name: 'Development'}
       ]);
     });
 };
--- database/seeds/02-Videos.js
+++ database/seeds/02-Videos.js
@@ -6,8 +6,13 @@ exports.seed = function(knex) {
         {id: 1, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 1},
         {id: 2, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 2},
         {id: 3, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
-        {id: 4, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 2},
-        {id: 5, Name: 'Example', Link: 'https://vimeo.com/506575905', Alt: 'This is the alt text...', Company: 3},
+        {id: 4, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
+        {id: 5, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
+        {id: 6, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 1},
+        {id: 7, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 2},
+        {id: 8, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
+        {id: 9, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
+        {id: 10, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
       ]);
     });
 };
