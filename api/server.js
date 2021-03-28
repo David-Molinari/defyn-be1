6f56b9650d586a54226bcd2e71e661af4268acc9
David Molinari <david.m.molinari@gmail.com>
2021-03-27 22:19:38 -0400

    Added gitignore

--- api/server.js
+++ api/server.js
@@ -21,14 +21,6 @@ const corsOptions = {
         }
 }
 
-// server.use((req, res, next) => {
-//         res.header("Access-Control-Allow-Origin", '*');
-//         res.header("Access-Control-Allow-Credentials", true);
-//         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//         res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//         next();
-// })
-
 server.use(helmet());
 server.use(cors(corsOptions));
 server.use(express.json());
--- secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || "keep it secret, keep it safe!",
-    mailPw: "TempPass.11"
-  };
