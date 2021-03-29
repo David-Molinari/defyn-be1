61c857db83b698bb5f3fe86a057ccfd02299741c
David Molinari <david.m.molinari@gmail.com>
2021-03-29 11:56:07 -0400

    Removed User files

--- models/Users.js
+++ models/Users.js
@@ -1,37 +1,37 @@
-const db = require("../database/dbConfig.js");
+// const db = require("../database/dbConfig.js");
 
-module.exports = {
-  find,
-  add,
-  changeEm,
-  changePw,
-  changeUn,
-  del
-};
+// module.exports = {
+//   find,
+//   add,
+//   changeEm,
+//   changePw,
+//   changeUn,
+//   del
+// };
 
-function find(insert) {
-    return db("Users").where(insert);
-}
+// function find(insert) {
+//     return db("Users").where(insert);
+// }
 
-function add(insert) {
-    return db("Users").insert(insert);
-}
+// function add(insert) {
+//     return db("Users").insert(insert);
+// }
 
-function changeEm(insert) {
-    return db("Users").where({Email: insert.Email, Company: insert.Company})
-        .update({ Email: insert.NewEmail })
-}
+// function changeEm(insert) {
+//     return db("Users").where({Email: insert.Email, Company: insert.Company})
+//         .update({ Email: insert.NewEmail })
+// }
 
-function changePw(insert) {
-    return db("Users").where({Company: insert.Company, Email: insert.Email})
-        .update({ Password: insert.hash })
-}
+// function changePw(insert) {
+//     return db("Users").where({Company: insert.Company, Email: insert.Email})
+//         .update({ Password: insert.hash })
+// }
 
-function changeUn(insert) {
-    return db("Users").where({Email: insert.Email, Company: insert.Company})
-        .update({ Username: insert.Username })
-}
+// function changeUn(insert) {
+//     return db("Users").where({Email: insert.Email, Company: insert.Company})
+//         .update({ Username: insert.Username })
+// }
 
-function del(insert) {
-    return db("Users").where("id", insert).del();
-}
+// function del(insert) {
+//     return db("Users").where("id", insert).del();
+// }
