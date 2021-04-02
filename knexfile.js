a66b3694b80d892cc534612b74ecc0c0c75bd702
David Molinari <david.m.molinari@gmail.com>
2021-04-02 14:30:31 -0400

    Updates to solve CORS errors

--- knexfile.js
+++ knexfile.js
@@ -36,6 +36,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './database/migrations'
     },
