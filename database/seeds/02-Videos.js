9384f328b653cb457ff2bb28f378c7e4a61fe90b
David Molinari <david.m.molinari@gmail.com>
2021-04-05 11:42:39 -0400

    Added cleaner

--- database/seeds/02-Videos.js
+++ database/seeds/02-Videos.js
@@ -1,18 +1,15 @@
 
 exports.seed = function(knex) {
-  return knex('Videos').del()
-    .then(function () {
-      return knex('Videos').insert([
-        {id: 1, Name: 'About', Link: 'https://vimeo.com/190506148', Alt: 'This is the alt text...', Company: 1},
-        {id: 2, Name: 'About1', Link: 'https://vimeo.com/358629078', Alt: 'This is the alt text...', Company: 2},
-        {id: 3, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
-        {id: 4, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
-        {id: 5, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
-        {id: 6, Name: 'Example', Link: 'https://vimeo.com/192593280', Alt: 'This is the alt text...', Company: 1},
-        {id: 7, Name: 'Example1', Link: 'https://vimeo.com/190646235', Alt: 'This is the alt text...', Company: 2},
-        {id: 8, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
-        {id: 9, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
-        {id: 10, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
-      ]);
-    });
+  return knex('Videos').insert([
+    {id: 1, Name: 'About', Link: 'https://vimeo.com/190506148', Alt: 'This is the alt text...', Company: 1},
+    {id: 2, Name: 'About1', Link: 'https://vimeo.com/358629078', Alt: 'This is the alt text...', Company: 2},
+    {id: 3, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
+    {id: 4, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
+    {id: 5, Name: 'About', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
+    {id: 6, Name: 'Example', Link: 'https://vimeo.com/192593280', Alt: 'This is the alt text...', Company: 1},
+    {id: 7, Name: 'Example1', Link: 'https://vimeo.com/190646235', Alt: 'This is the alt text...', Company: 2},
+    {id: 8, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 3},
+    {id: 9, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 4},
+    {id: 10, Name: 'Example', Link: 'https://vimeo.com/510614745', Alt: 'This is the alt text...', Company: 5},
+  ]);
 };
--- database/seeds/03-Meetings.js
+++ database/seeds/03-Meetings.js
@@ -1,10 +1,7 @@
 
 exports.seed = function(knex) {
-  return knex('Meetings').del()
-    .then(function () {
-      return knex('Meetings').insert([
-        {id: 1, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617635745', EndTime: '1617636645', Company: 2},
-        {id: 2, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617636645', EndTime: '1617637500', Company: 2}
-      ]);
-    });
+  return knex('Meetings').insert([
+    {id: 1, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617635745', EndTime: '1617636645', Company: 2},
+    {id: 2, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617636645', EndTime: '1617637500', Company: 2}
+  ]);
 };
