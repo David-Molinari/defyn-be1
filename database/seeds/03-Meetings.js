a7ad4eeac2bc7ea7aacb1f550fb0ec61aba829c9
David Molinari <david.m.molinari@gmail.com>
2021-04-05 10:50:59 -0400

    Updating seeds

--- database/seeds/03-Meetings.js
+++ database/seeds/03-Meetings.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
   return knex('Meetings').del()
     .then(function () {
       return knex('Meetings').insert([
-        {id: 1, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617635745', EndTime: '1617636645', Company: 5},
-        {id: 2, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617636645', EndTime: '1617637500', Company: 5}
+        {id: 1, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617635745', EndTime: '1617636645', Company: 2},
+        {id: 2, LeadEmail: '', LeadName: '', LeadCompany: '', StartTime: '1617636645', EndTime: '1617637500', Company: 2}
       ]);
     });
 };
