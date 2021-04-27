d3b8e316b1f85c0dc342095dc754da8aa5c22a24
David Molinari <david.m.molinari@gmail.com>
2021-04-27 13:34:25 -0400

    Updates

--- database/seeds/01-Companies.js
+++ database/seeds/01-Companies.js
@@ -1,8 +1,5 @@
 exports.seed = function(knex) {
   return knex('Companies').insert([
-    {id: 1, URL: 'defyn.co', Name: 'Defyn', Email: 'hi@defyn.co', StripeID: '', VideoOrder: ' 1 3 ', Price: ''},
-    {id: 2, URL: 'localhost:3000', Name: 'Development', Email: 'd_mm9152@yahoo.com', StripeID: 'acct_1IgEu0BRD7BJ4mUo', VideoOrder: ' 2 4 ', Price: '1000'}
+    {id: 1, URL: 'davidmolinari.me', Name: 'David Molinari', Email: 'david.m.molinari@gmail.com', StripeID: '', VideoOrder: ' 1 ', Price: ''}
   ]);
 };
-
-// stripe account: acct_1IgEu0BRD7BJ4mUo
--- database/seeds/02-Videos.js
+++ database/seeds/02-Videos.js
@@ -1,9 +1,5 @@
-
 exports.seed = function(knex) {
   return knex('Videos').insert([
     {id: 1, Name: 'About', Link: 'https://vimeo.com/190506148', Unpaid: true, Company: 1},
-    {id: 2, Name: 'About', Link: 'https://vimeo.com/510614745', Unpaid: true, Company: 2},
-    {id: 3, Name: 'Example', Link: 'https://vimeo.com/192593280', Unpaid: true, Company: 1},
-    {id: 4, Name: 'Example', Link: 'https://vimeo.com/510614745', Unpaid: true, Company: 2}
   ]);
 };
