533bc49bcf31a3e9eb88878f61b6bcad70dae287
David Molinari <david.m.molinari@gmail.com>
2021-04-27 15:04:01 -0400

    Update

--- database/migrations/20210409113945_Codes.js
+++ database/migrations/20210409113945_Codes.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema
       .createTable("Codes", (tbl) => {
         tbl.increments("id").unique().notNullable()
-        tbl.integer("Code").unique().notNullable()
+        tbl.string("Code").unique().notNullable()
         tbl.string("Email").references("Email").inTable("Companies").notNullable()
           .onUpdate("CASCADE")
           .onDelete("CASCADE")
