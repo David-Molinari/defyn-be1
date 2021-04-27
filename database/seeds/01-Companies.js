32bf4c8fd438c6903553a1cdbb0e18675482b945
David Molinari <david.m.molinari@gmail.com>
2021-04-27 13:54:21 -0400

    Updates

--- database/seeds/01-Companies.js
+++ database/seeds/01-Companies.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex) {
   return knex('Companies').insert([
-    {id: 1, URL: 'davidmolinari.me', Name: 'David Molinari', Email: 'david.m.molinari@gmail.com', StripeID: '', VideoOrder: ' 1 ', Price: ''}
+    {id: 1, URL: 'defyn.co', Name: 'Defyn', Email: 'david.m.molinari@gmail.com', StripeID: '', VideoOrder: ' 1 ', Price: ''}
   ]);
 };
--- database/seeds/02-Videos.js
+++ database/seeds/02-Videos.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex) {
   return knex('Videos').insert([
-    {id: 1, Name: 'About', Link: 'https://vimeo.com/190506148', Unpaid: true, Company: 1},
+    {id: 1, Name: 'About', Link: 'https://vimeo.com/192511901', Unpaid: true, Company: 1},
   ]);
 };
